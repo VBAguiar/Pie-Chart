f5cb4102d1c9417ae71bc2f9adea76f9dec4d9f8
vinicius barbosa de aguiar <69726088+ward910@users.noreply.github.com>
2021-08-04 09:06:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+# TiTle
 plt.title('Pie Chart')
 
+# Percentage
 y = np.array([25, 25, 25, 25])
 
+# Name && Separation
 mylabels = ['Apple', 'Bananas', 'Orange', 'Guava']
 myexplode = [0, 0, 0, 0]
 
+# Run
 plt.pie(y, labels=mylabels, explode=myexplode, shadow=True)
 plt.show()
